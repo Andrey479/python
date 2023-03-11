eeb4747a842f956f64a23a973bf98d49c1469bab
Andrey Oliveira <122578824+Andrey479@users.noreply.github.com>
2023-03-10 22:48:57 -0300

    Delete 20230301-115551.py

--- 20230301-115551.py
+++
@@ -1,8 +0,0 @@
-#número total de elementos pares na lista
-A=[]
-par=0
-for i in range(0,15):
-    A.append(int(input('Digite um valor: ')))
-    if A[i]%2==0:
-        par+=1
-print (f'Existem: {par} elementos pares na matriz')
