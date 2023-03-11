560cac39d735a68bdc45402a8f7e95e4fa6509f7
Andrey Oliveira <122578824+Andrey479@users.noreply.github.com>
2023-03-10 22:49:18 -0300

    Delete 20230302-103548.py

--- 20230302-103548.py
+++
@@ -1,9 +0,0 @@
-#porcentagem de n?meros impares em uma lista de 10 elementos
-A=[]
-impar=0
-for i in range(0,10):
-    A.append(int(input('Digite um valor: ')))
-    if not A[i]%2==0:
-        impar+=1
-porcentagem=impar*100/len(A)
-print (f'Na matriz há: {impar} elementos impares, que correspondem a {porcentagem}% do total de elementos.')
